aa99f25a9a8168e98f54d01674d81f92e6f022d3
adarsh-shahi <adarshshahi1009@gmail.com>
2022-10-10 17:28:24 +0530

    init

---
+++ classes.js
@@ -0,0 +1,17 @@
+
+//https://www.freecodecamp.org/news/how-javascript-implements-oop/
+
+/*
+JavaScript isn't a classed-based langauge – it's is a prototype-based langauge
+*/
+
+const names = { 
+    fNmae: 'adarsh',
+    lName: 'shahi'
+}
+
+console.log(names);
+console.log(names.hasOwnProperty('fName'));
+
+ // fNmae: "adarsh" lName: "shahi"
+ 
