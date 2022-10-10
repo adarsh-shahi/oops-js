e770453134ba797c2124d2eea71cbe68faa58f8a
adarsh-shahi <adarshshahi1009@gmail.com>
2022-10-10 17:56:11 +0530

    constructor function

--- classes.js
+++ classes.js
@@ -14,4 +14,20 @@ console.log(names);
 console.log(names.hasOwnProperty('fName'));
 
  // fNmae: "adarsh" lName: "shahi"
+// Constructor function
+
+const Person = function(firstName, lastName){ // hmm looks like a normal function
+    this.firstName = firstName;
+    this.lastName = lastName;
+}
+
+// lets call it with 'new' keyword
+// 4 things happen 
+// 1. new empty object is created {}
+// 2. function is called, this = {}
+// 3. {} linked to prototype
+// 4. function automatically returns {}
+
+const adarsh = new Person('adarsh', 'shahi')
+console.log(adarsh);
  
